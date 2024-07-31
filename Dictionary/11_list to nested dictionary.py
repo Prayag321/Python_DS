f955d0093613c31dbb1b6cceee513d86bc6c2391
PRAYAG <nanabhoir321@gmail.com>
2024-07-31 06:50:49 +0530

    [PRAYAG] Refactor:Python program  to convert a list into a nested dictionary of keys

--- Dictionary/11_list to nested dictionary.py
+++ Dictionary/11_list to nested dictionary.py
@@ -2,18 +2,19 @@
     @Author: Prayag Bhoir
     @Date: 30-07-2024 
     @Last Modified by: Prayag Bhoir
-    @Last Modified time: 30-07-2024 
+    @Last Modified time: 31-07-2024 
     @Title : Python program  to convert a list into a nested dictionary of keys
 """
 def list_to_nested_dict(input_list):
     """
-    Converts a list into a nested dictionary of keys.
+    Description:
+        Converts a list into a nested dictionary of keys.
 
     Parameters:
-    input_list (list): A list of elements
+        input_list (list): A list of elements
 
     Returns:
-    dict: A nested dictionary 
+        dict: A nested dictionary 
     """
     nested_dict = current_level = {}
     for element in input_list:
