412fd3612769367212f0b431e6400bc8d81b52b3
PRAYAG <nanabhoir321@gmail.com>
2024-07-31 07:16:29 +0530

    [PRAYAG] Add: Python program to create a tuple with different data types

---
+++ Tuple/2_create a tuple with different data types.py
@@ -0,0 +1,13 @@
+'''
+    @Author: Prayag Bhoir
+    @Date: 29-07-2024 
+    @Last Modified by: Prayag Bhoir
+    @Last Modified time: 29-07-2024 
+    @Title :Python program to create a tuple with different data types.
+'''
+
+def main():
+  tlp=tuple(["prayag",22,True])
+  print("We just create a tuple",tlp)
+if __name__=="__main__":
+  main()
